222af377f082710b30816b880e992c40e876f99b
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-01 23:36:22 -0500

    commit 2

--- truffle-config.js
+++ truffle-config.js
@@ -59,7 +59,7 @@ module.exports = {
     },
     //for use with local environment -- use `npm runLocalArbitrum` to start
     ethereum_local: {
-      network_id: '*',
+      network_id: '5777',
       host: '127.0.0.1',
       port: 7545,
       gasPrice: 0
@@ -74,7 +74,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.6",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.7",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
